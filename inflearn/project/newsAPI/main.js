4bd91ceea2a768427699ff1051b33954d68deba5
silvermaking <silvermaking@gmail.com>
2022-05-03 08:57:28 +0900

    inflearn: api project-mid

--- inflearn/project/newsAPI/main.js
+++ inflearn/project/newsAPI/main.js
@@ -1,8 +1,12 @@
 let news = [];
-const getLatestNews = async () => {
-  let url = new URL(
-    "https://api.newscatcherapi.com/v2/latest_headlines?countries=US&topic=business&page_size=2"
-  );
+let menus = document.querySelectorAll(".menus button");
+let searchButton = document.getElementById("search-button");
+
+menus.forEach((menu) => {
+  menu.addEventListener("click", (event) => getNewsByTopic(event));
+});
+
+const getNews = async (url) => {
   let header = new Headers({
     "x-api-key": "ERykQRI3guOipZkPDSzaPa8gKYqdu1DbADMrQXw2Orw",
   });
@@ -10,7 +14,56 @@ const getLatestNews = async () => {
   // response 결과 데이터로 뽑아오기
   let data = await response.json();
   news = data.articles;
-  console.log("this is news", news);
+
+  render();
 };
 
+const getLatestNews = async () => {
+  let url = new URL(
+    "https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&page_size=10"
+  );
+  getNews(url);
+};
+
+const getNewsByKeyword = async () => {
+  // 1. 검색 키워드 읽어오기
+  const keyword = document.getElementById("search-input").value;
+  console.log("keyword", keyword);
+  // 2. url에 검색 키워드 넣기
+  const url = new URL(
+    `https://api.newscatcherapi.com/v2/search?q=${keyword}&countries=KR&page_size=10`
+  );
+  getNews(url);
+};
+
+const getNewsByTopic = async (event) => {
+  let topic = event.target.textContent.toLowerCase();
+  let url = new URL(
+    `https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&page_size=10&topic=${topic}`
+  );
+  getNews(url);
+};
+
+const render = () => {
+  let newsHTML = "";
+  newsHTML = news.map((item) => {
+    return `<div class="row news">
+    <div class="col-lg-4">
+      <img
+        class="news-img-size"
+        src="${item.media}"
+        alt=""
+      />
+    </div>
+    <div class="col-lg-8">
+      <h2>${item.title}</h2>
+      <p>${item.summary}</p>
+      <div>${item.rights} ${item.published_date}</div>
+    </div>
+  </div>`;
+  });
+  document.getElementById("news-board").innerHTML = newsHTML.join("");
+};
+
+searchButton.addEventListener("click", getNewsByKeyword);
 getLatestNews();
